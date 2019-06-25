ea0267644dcad6e6356e81fd83d173965488aeb9
hi631 <hi631@ac.auone-net.jp>
2019-06-25 22:52:00 +0900

    add mnist

---
+++ floatconv/Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Tex1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Pic1 = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Text2 = New System.Windows.Forms.TextBox()
+        CType(Me.Pic1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Tex1
+        '
+        Me.Tex1.Location = New System.Drawing.Point(12, 12)
+        Me.Tex1.Name = "Tex1"
+        Me.Tex1.Size = New System.Drawing.Size(280, 19)
+        Me.Tex1.TabIndex = 0
+        Me.Tex1.Text = "..\..\data\0_1.png"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(298, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(42, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "変換"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Pic1
+        '
+        Me.Pic1.Location = New System.Drawing.Point(12, 76)
+        Me.Pic1.Name = "Pic1"
+        Me.Pic1.Size = New System.Drawing.Size(280, 280)
+        Me.Pic1.TabIndex = 2
+        Me.Pic1.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(299, 41)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(41, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "確認"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Text2
+        '
+        Me.Text2.Location = New System.Drawing.Point(12, 41)
+        Me.Text2.Name = "Text2"
+        Me.Text2.Size = New System.Drawing.Size(280, 19)
+        Me.Text2.TabIndex = 4
+        Me.Text2.Text = "..\..\data\0_1.bin"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(347, 368)
+        Me.Controls.Add(Me.Text2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Pic1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Tex1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Pic1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Tex1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Pic1 As PictureBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Text2 As TextBox
+End Class
---
+++ floatconv/Form1.vb
@@ -0,0 +1,74 @@
+﻿Imports System.Drawing
+Public Class Form1
+    Dim ByteArray() As Byte
+    Private Sub getbf()
+        Dim fname As String = Text2.Text
+        Dim fs As New System.IO.FileStream(fname, System.IO.FileMode.Open, System.IO.FileAccess.Read)
+        ReDim ByteArray(fs.Length - 1)
+        fs.Read(ByteArray, 0, ByteArray.Length)
+        fs.Close()
+    End Sub
+    Private Sub dispbmp()
+        Dim canvas As New Bitmap(Pic1.Width, Pic1.Height)
+        Dim g As Graphics = Graphics.FromImage(canvas)
+        Dim xs, ys, xp, yp, c
+        Dim fd As Single
+        For ys = 0 To 27
+            For xs = 0 To 27
+                fd = BitConverter.ToSingle(ByteArray, (ys * 28 + xs) * 4)
+                yp = ys * 10
+                xp = xs * 10
+                c = fd And 255
+                Dim b As New SolidBrush(Color.FromArgb(255, c, c, c))
+                g.FillRectangle(b, xp, yp, xp + 10, yp + 10)
+                b.Dispose()
+            Next
+        Next
+        g.Dispose()
+        Pic1.Image = canvas
+    End Sub
+    Private Sub cnvp2s()
+        Dim canvas As New Bitmap(Pic1.Width, Pic1.Height)
+        Dim g As Graphics = Graphics.FromImage(canvas)
+
+        Dim img As Image = Image.FromFile(Tex1.Text)
+        Pic1.Image = img
+        Dim P As Bitmap
+        Dim bc As Color
+        Dim sc As Single
+        P = CType(Pic1.Image, Bitmap)
+
+        Dim fname As String = Text2.Text
+        Dim fs As New System.IO.FileStream(fname,
+    System.IO.FileMode.Create,
+    System.IO.FileAccess.Write)
+        ReDim ByteArray(3)
+
+        Dim xs, ys, xp, yp, c
+        For ys = 0 To 27
+            For xs = 0 To 27
+                yp = ys * 10
+                xp = xs * 10
+                bc = P.GetPixel(xs, ys)
+                c = bc.ToArgb() And 255
+                sc = c
+                ByteArray = BitConverter.GetBytes(sc)
+                fs.Write(ByteArray, 0, 4)
+                Dim b As New SolidBrush(Color.FromArgb(255, c, c, c))
+                g.FillRectangle(b, xp, yp, xp + 10, yp + 10)
+                b.Dispose()
+            Next
+        Next
+        Pic1.Image = canvas
+        fs.Close()
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        cnvp2s()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        getbf()
+        dispbmp()
+    End Sub
+End Class
