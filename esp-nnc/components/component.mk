67df01e458c11db9f32923d0f228ba19e5a0ccad
hi631 <hi631@ac.auone-net.jp>
2019-07-03 07:54:25 +0900

    Improvement

--- esp-nnc/components/component.mk
+++ esp-nnc/components/component.mk
@@ -1,4 +1,5 @@
 COMPONENT_ADD_INCLUDEDIRS := ../include
+#COMPONENT_SRCDIRS := implements utilities
 COMPONENT_SRCDIRS := implements utilities implements/activation implements/arithmetic implements/array implements/logical implements/math implements/neural_network implements/neural_network/affine implements/neural_network/convolution implements/normalization implements/quantization implements/reduction implements/stochasticity
 
 
